eadd4765d99d3f6fc0656fd50e6244f54aacbca9
Shane99dt <dushen1999thilaksha@gmail.com>
2022-09-02 16:07:31 +0200

    little

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// window.alert("This portfolio is still on the way to a better build. Enjoy your time here now!");
+window.alert("This portfolio is still on the way to a better build. Enjoy your time here now!");
+
+
+function onClickSend() {
+  alert("Please send an e-mail");
+}
